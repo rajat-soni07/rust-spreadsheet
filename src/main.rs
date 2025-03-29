f66fb212e1c8f676c7144bd66f5f0d983c4a8621
Rajat Soni <rajat.soni0707@gmail.com>
2025-03-29 17:35:49 +0530

    added basic display unit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod utils;
 
 fn main() {
-    utils::display::display();
+    let len_h :i32 = 10;
+    let len_v :i32 = 10;
+    let database = vec![0;(len_h*len_v + 1)as usize];
+    utils::display::display_grid(1,1,len_h,len_v,&database);
 }
--- src/utils/display.rs
+++ src/utils/display.rs
@@ -1,4 +1,54 @@
-pub fn display(){
-    println!("Display test");
+fn shift_char(c:char, i: i32)-> char{
+    (c as i8 + i as i8) as u8 as char
+}
+
+fn get_label(a : i32)-> String{
+    let mut temp = String::new();
+    let mut num = a-1;
+    if 0 <= num && num <= 25 {
+        temp.push(shift_char('A', num));
+    } else if 26 <= num && num <= 701 {
+        num-=26;
+        temp.push(shift_char('A', num/26));
+        temp.push(shift_char('A', num%26));
+    } else if 702 <= num && num <= 18277 {
+        num-=702;
+        let c = shift_char('A', num%26);
+        num/=26;
+        temp.push(shift_char('A', num/26));
+        temp.push(shift_char('A', num%26));
+        temp.push(c);
+    }
+
+    temp
+}
+
+pub fn display_grid(top_h: i32,top_v: i32,len_h: i32, len_v:i32, database: &Vec<i32>){
+    let i1 = top_h;
+    let mut i2 = top_h + 9;
+
+    if i2>len_h{i2=len_h;}
+
+    for i in i1..=i2 {
+        print!("\t{}",get_label(i));
+    }
+
+    println!();
+
+    let i3 = top_v;
+    let mut i4 = top_v + 9;
+
+    if i4>len_v {i4=len_v;}
+
+    for j in i3..=i4 {
+        print!("{j}");
+        for i in i1..=i2{
+            print!("\t{}",database[((j-1)*len_h + i) as usize]);
+        }
+        println!();
+    }
+
+
+
     return;
 }
